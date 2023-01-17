b3f5a344c6ffb274ec6eec861f49df067b832ff8
oscalenghe <42279508+oscalenghe@users.noreply.github.com>
2023-01-17 16:42:30 -0500

    added ids for main forecast

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,9 @@
 var apiKey = "26554d07776928bf078f6e60f3c5c2b7";
+var searchFormEl = document.getElementById('search-box');
+
+// IDs for current weather
+var cityName = document.getElementById('current-city');
+var todaysDate = document.getElementById('current-date');
+var todaysTemp = document.getElementById('temperature'); 
+var todaysHumidity = document.getElementById('humidity');
+var todaysWind = document.getElementById('wind-speed');
