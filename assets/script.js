000b7a25c48e40b56e61309987d8411d342e6af3
oscalenghe <42279508+oscalenghe@users.noreply.github.com>
2023-01-17 16:49:59 -0500

    added ids for future forecast

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,35 @@ var todaysDate = document.getElementById('current-date');
 var todaysTemp = document.getElementById('temperature'); 
 var todaysHumidity = document.getElementById('humidity');
 var todaysWind = document.getElementById('wind-speed');
+
+
+// IDs for 5-day forecast
+var fDate1 = document.getElementById('day1');
+var fImg1 = document.getElementById('fImg1');
+var fTemp1 = document.getElementById('fTemp1'); 
+var fWind1 = document.getElementById('fWind1');
+var fHumidity1 = document.getElementById('fHumidity1');
+
+var fDate2 = document.getElementById('day2');
+var fImg2 = document.getElementById('fImg2');
+var fTemp2 = document.getElementById('fTemp2'); 
+var fWind2 = document.getElementById('fWind2');
+var fHumidity2 = document.getElementById('fHumidity2');
+
+var fDate3 = document.getElementById('day3');
+var fImg3 = document.getElementById('fImg3');
+var fTemp3 = document.getElementById('fTemp3'); 
+var fWind3 = document.getElementById('fWind3');
+var fHumidity3 = document.getElementById('fHumidity3');
+
+var fDate4 = document.getElementById('day4');
+var fImg4 = document.getElementById('fImg4');
+var fTemp4 = document.getElementById('fTemp4'); 
+var fWind4 = document.getElementById('fWind4');
+var fHumidity4 = document.getElementById('fHumidity4');
+
+var fDate5 = document.getElementById('day5');
+var fImg5 = document.getElementById('fImg5');
+var fTemp5 = document.getElementById('fTemp5'); 
+var fWind5 = document.getElementById('fWind5');
+var fHumidity5 = document.getElementById('fHumidity5');
